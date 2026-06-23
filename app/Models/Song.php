b7c4440262ac3daedc58756dd0a78f315f697067
https://github.com/davegulo145-a11y/express-template-msc/blob/main/index.js <davegulo145@gmail.com>
2026-06-23 21:02:26 +0700

    Fix Song model relationships

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -15,7 +15,17 @@ class Song extends Model
     ];
 
     public function favorites()
-{
-    return $this->hasMany(Favorite::class);
-}
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function playlists()
+    {
+        return $this->belongsToMany(Playlist::class);
+    }
 }
